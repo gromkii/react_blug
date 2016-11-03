c874cc9f8bfea3187faf04c2aa84fb5fc2581b4c
Dax <limeside@gmail.com>
2016-11-03 13:56:35 -0500

    Add dependencies into server. :rabbit:

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-var express = require('express');
+var express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override')
+  mongoose = require('mongoose');
 
-// Create our app
-var app = express();
+mongoose.connect('mongodb://localhost/react_blug');
 
+// Middleware
+app.use(bodyParser.JSON());
+app.use(bodyParser({extended:true});
+app.use(methodOverride('_method');
 app.use(express.static('public'));
 
 app.listen(3000, function () {
